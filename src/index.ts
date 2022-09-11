8faf640b44acd8d8d10973ce30e439721a7f4061
Yuriy Oliferchuk <dbzero86@gmail.com>
2022-09-11 08:34:16 +0300

    start

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/samurais', (req, res) => {
+  res.send('Hello Samurais!')
+})
+
+app.post('/samurais', (req, res) => {
+  res.send('We have created Samurai!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port \nhttp://localhost:${port}`)
+})
